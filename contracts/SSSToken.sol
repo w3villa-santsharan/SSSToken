0d848439a1fa16cbcb78bdcd09b6aafe74939048
w3villa-santsharan <sant.shukla@w3villa.com>
2022-06-08 16:10:31 +0530

    mint and totalsupply provided

--- contracts/SSSToken.sol
+++ contracts/SSSToken.sol
@@ -1,15 +1,35 @@
 // SPDX-License-Identifier: None
 
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
-contract SSSToken is ERC20 {
- 
+contract SSSToken is ERC20, Ownable {
 
-    constructor () ERC20 ("SSSTokenName", "SSS") {
-     
+using SafeMath for uint256;
+uint256 private  _decimals = 18;
+uint256 private  _cap = 10000 * (10 ** _decimals); 
+uint256 public _totalSupply;
+
+    constructor () ERC20 ("SSSToken", "SSS") {
+      uint256 initialSupply = 1000;
+      
+      _mint(msg.sender, initialSupply);
+         _totalSupply = initialSupply;
     }
+
+      function mint(address account, uint256 amount) external onlyOwner  {
+      require(_totalSupply.add(amount) <= _cap, "ERC20: cap exceeded");
+
+      _totalSupply += amount;
+      _mint(account, amount);
+  }
+
+
+
+    
 }
 
